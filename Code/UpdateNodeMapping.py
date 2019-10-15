0fb662a5143278e146c21d1d06573bdef2cd15ec
Raj Biswas <rajbiswas994@gmail.com>
2019-10-15 10:02:50 -0400

    Corrected C initialization

--- Code/UpdateNodeMapping.py
+++ Code/UpdateNodeMapping.py
@@ -1,5 +1,11 @@
 import networkx as nx
 import numpy as np
 
-def InitializeMapping(n_t, p):
-	return np.random.randint(low = 0, high = 2, size = (n_t, p))
+def InitializeMapping(sim, p):
+	C = []
+	for i in range(len(sim)):
+		C_t = np.zeros((sim[i].shape[0], p[i]))
+		for j in range(sim[i].shape[0]):
+			C_t[j, np.random.randint(low = 0, high = p[i])] = 1
+			C.append(C_t)
+	return C
