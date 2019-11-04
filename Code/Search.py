37a27f0b92d119f53b984beae693020d1531e14c
LisaThomas9 <lisasarahthomas@gmail.com>
2019-11-03 23:26:04 -0500

    added objective function

--- Code/Search.py
+++ Code/Search.py
@@ -7,6 +7,30 @@ def ComputeTheta(C, k_type):
 	theta = min(vertex_cluster_contribution_sum)
 	return theta, vertex_cluster_contribution_sum
 
+def ComputeObjectiveFunction(S, C, L, CN_sim):
+	#k_type = 2
+	for type_ in range(k_type):
+		sim_t = CN_sim[type_]
+		C_t = C[type_]
+		n_t = len(sim_t)
+
+		first_term = 0
+		for i in range(n_t):
+			for j in range(i):
+				first_term += sim_t[i][j]*(C_t[i] - C_t[j])**2 ### doubt
+
+		second_term = 0
+		for t in range():
+			for t_dash in range():
+				#create subgraph G_t_t_dash
+				temp1 = np.matmul(C[t], L[t][t_dash])
+				temp2 = np.matmul(temp1, C[t_dash].transpose())
+
+				second_term += (G_t_t_dash - temp2)**2 ### doubt
+
+		objective = first_term + second_term
+		return objective
+	
 
 def GetOptimalSuperNode(G, SG, S, C):
 	#S is a list of dictionaries; C is a list of numpy matrices
