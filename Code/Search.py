64177ce7ecc75833633a89469bf9f8b574e588ef
Neha Saraf <neha.94saraf@gmail.com>
2019-12-01 13:11:49 -0500

    Added Calculation of Objective Function

--- Code/Search.py
+++ Code/Search.py
@@ -6,28 +6,28 @@ def computeTheta(C_t):
     theta = min(vertex_cluster_contribution_sum)
     return theta, vertex_cluster_contribution_sum
 
-# def computeObjectiveFunction(C, L, V, CN_sim, G):
-#     k_type = len(C)
-#     first_term, second_term = 0, 0
-#     for type_ in range(k_type):
-#         sim_t = CN_sim[type_]
-#         C_t = C[type_]
-#         n_t = len(sim_t)#n_t: number of vertices in the type
+def computeObjectiveFunction(C, L, V, CN_sim, G):
+    k_type = len(C)
+    first_term, second_term = 0, 0
+    for type_ in range(k_type):
+        sim_t = CN_sim[type_]
+        C_t = C[type_]
+        n_t = len(sim_t)#n_t: number of vertices in the type
 
-#         for i in range(n_t):
-#             for j in range(i):
-#                 first_term += sim_t[i][j]*np.sum((C_t[i] - C_t[j])**2)
+        for i in range(n_t):
+            for j in range(i):
+                first_term += sim_t[i][j]*np.sum((C_t[i] - C_t[j])**2)
 
-#     for t in range(k_type):
-#         for t_dash in range(t+1,k_type):
-#             G_t_t_dash = Gtt[(t,t_dash)]
-#             temp1 = np.matmul(L[(t, t_dash)]['adj_matrix'],C[t_dash].transpose())
-#             temp2 = np.matmul(C[t], temp1)
+    for t in range(k_type):
+        for t_dash in range(t+1,k_type):
+            G_t_t_dash = Gtt[(t,t_dash)]
+            temp1 = np.matmul(L[(t, t_dash)]['adj_matrix'],C[t_dash].transpose())
+            temp2 = np.matmul(C[t], temp1)
 
-#             second_term += np.sum((G_t_t_dash - temp2)**2)
+            second_term += np.sum((G_t_t_dash - temp2)**2)
 
-#     objective = first_term + second_term
-#     return objective
+    objective = first_term + second_term
+    return objective
 
 def removeSuperNodeFromL(L, t_type, supernode, index):
     L_temp = copy.deepcopy(L)
