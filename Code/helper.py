d08acfc36142419a0b544e39d9533c3db32958cf
LisaThomas9 <lisasarahthomas@gmail.com>
2019-12-06 08:57:31 -0500

    added objective function

--- Code/helper.py
+++ Code/helper.py
@@ -14,4 +14,25 @@ def calculate_diagonal_matrix(sim):
     
     return D
 
-D = calcDiagonalMatrix(CN_sim)
+def computeObjectiveFunction(C, L, V, CN_sim, G):
+    k_type = len(C)
+    first_term, second_term = 0, 0
+    for type_ in range(k_type):
+        sim_t = CN_sim[type_]
+        C_t = C[type_]
+        n_t = len(sim_t)#n_t: number of vertices in the type
+
+        for i in range(n_t):
+            for j in range(i):
+                first_term += sim_t[i][j]*np.sum((C_t[i] - C_t[j])**2)
+
+    for t in range(k_type):
+        for t_dash in range(t+1,k_type):
+            G_t_t_dash = Gtt[(t,t_dash)]
+            temp1 = np.matmul(L[(t, t_dash)]['adj_matrix'],C[t_dash].transpose())
+            temp2 = np.matmul(C[t], temp1)
+
+            second_term += np.sum((G_t_t_dash - temp2)**2)
+
+    objective = first_term + second_term
+    return objective
