132b55ba6e439cc94503b5943274f868c9044088
LisaThomas9 <lisasarahthomas@gmail.com>
2019-12-07 02:20:12 -0500

    fixed the objective function

--- Code/helper.py
+++ Code/helper.py
@@ -19,29 +19,28 @@ def createJaccardSim(graph):
     return similarity_matrix
 
 def calculate_diagonal_matrix(similarity_matrix):
-    D=[]
+    diagonal_matrix = []
     for index, type_i_sim_matrix in enumerate(similarity_matrix):
-        D.append(np.diag(np.sum(type_i_sim_matrix, axis=1)))
+        diagonal_matrix.append(np.diag(np.sum(type_i_sim_matrix, axis=1)))
     
-    return D
+    return diagonal_matrix
 
-def computeObjectiveFunction(C, L, V, CN_sim, G):
-    k_type = len(C)
+def computeObjectiveFunction(graph, c_mapping, superlinks, similarity_matrix):
     first_term, second_term = 0, 0
-    for type_ in range(k_type):
-        sim_t = CN_sim[type_]
-        C_t = C[type_]
+    for type_i in range(graph.types):
+        sim_t = similarity_matrix[type_i]
+        C_t = c_mapping.C[type_i]
         n_t = len(sim_t)#n_t: number of vertices in the type
 
-        for i in range(n_t):
-            for j in range(i):
-                first_term += sim_t[i][j]*np.sum((C_t[i] - C_t[j])**2)
+        for vertex_i in range(n_t):
+            for vertex_j in range(i):
+                first_term += sim_t[vertex_i][vertex_j]*np.sum((C_t[vertex_i] - C_t[vertex_j])**2)
 
-    for t in range(k_type):
-        for t_dash in range(t+1,k_type):
-            G_t_t_dash = Gtt[(t,t_dash)]
-            temp1 = np.matmul(L[(t, t_dash)]['adj_matrix'],C[t_dash].transpose())
-            temp2 = np.matmul(C[t], temp1)
+    for t in range(graph.types):
+        for t_dash in range(t+1, graph.types):
+            G_t_t_dash = graph.Gtt[(t,t_dash)]
+            temp1 = np.matmul(superlinks.L[(t, t_dash)]['adj_matrix'], c_mapping.C[t_dash].transpose())
+            temp2 = np.matmul(c_mapping.C[t], temp1)
 
             second_term += np.sum((G_t_t_dash - temp2)**2)
 
