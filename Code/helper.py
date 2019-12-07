f6ab631e186e8c251514698ec3a333f655a020ae
LisaThomas9 <lisasarahthomas@gmail.com>
2019-12-07 03:04:04 -0500

    added algorithm 2

--- Code/helper.py
+++ Code/helper.py
@@ -25,7 +25,7 @@ def calculate_diagonal_matrix(similarity_matrix):
     
     return diagonal_matrix
 
-def computeObjectiveFunction(graph, c_mapping, superlinks, similarity_matrix):
+def compute_objective_function(graph, c_mapping, superlinks, similarity_matrix):
     first_term, second_term = 0, 0
     for type_i in range(graph.types):
         sim_t = similarity_matrix[type_i]
@@ -46,3 +46,34 @@ def computeObjectiveFunction(graph, c_mapping, superlinks, similarity_matrix):
 
     objective = first_term + second_term
     return objective
+
+def compute_theta(C_t):
+    vertex_cluster_contribution_sum = np.sum(C_t, axis = 0)
+    theta = min(vertex_cluster_contribution_sum)
+    return theta, vertex_cluster_contribution_sum
+
+def remove_supernode_from_L(superlinks, t_type, index_to_remove):
+    for types, links in superlinks.L.items():
+        if types[0] > t_type:
+            break
+        elif types[0] == t_type:
+            links['adj_matrix'] = np.delete(links['adj_matrix'], index_to_remove , 0)
+            #superlinks[types[0]].remove(supernode)
+        
+        elif types[1] == t_type:
+            links['adj_matrix'] = np.delete(links['adj_matrix'], index_to_remove , 1)
+            #superlinks[types[1]].remove(supernode)
+
+def get_optimal_supernode(G, SG, S, C, L, V, CN_sim): (graph, summary_graph, c_mapping, superlinks, similarity_matrix)
+
+    for type_i in range(graph.types):
+        theta, vertex_cluster_contribution_sum = compute_theta(c_mapping.C[type_i])
+        min_info_index = np.argmin(vertex_cluster_contribution_sum)
+        #name_node = S[i]['name'][index]
+        c_mapping.C[type_i] = np.delete(c_mapping.C[type_i], min_info_index, 1)
+        remove_supernode_from_L(superlinks, type_i, min_info_index) #does it work in-place?
+    
+    # start_time = time.time()
+    # SG, S = createSummaryGraph(G, C, V, k_type)
+    # print("Time taken to update the summary graph:", (time.time()-start_time)/60)
+    return C, L
